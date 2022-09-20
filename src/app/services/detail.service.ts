a3c6e5da87fe1ca55c9afd2d7943141393bdc571
nigussolomon444 <nigussolomon444@gmail.com>
2022-09-20 07:22:07 +0300

    third iteration complete

--- src/app/services/detail.service.ts
+++ src/app/services/detail.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailService {
+  private detailUrl = 'https://edi-api-test-12.herokuapp.com/get_po/?link='
 
-  private detailUrl = 'https://edi-api-test-12.herokuapp.com?link='
-   
-  constructor(private httpClient: HttpClient) { }
+  constructor (private httpClient: HttpClient) {}
 
-  getDetails(link?: any){ 
-   return this.httpClient.get(this.detailUrl + link)
+  getDetails (link?: any, po_s?: any, po_n?: any, big?: any) {
+    return this.httpClient.get(this.detailUrl + link)
   }
 }
